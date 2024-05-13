ff3e6897d97e91dc291e9d26dfa85a186e99714b
Luis Guilherme <111072200+LGuilher2003@users.noreply.github.com>
2024-05-13 09:24:31 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Button, Image, ImageBackground, StyleSheet, Text, TextInput, View,SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import eventos from './eventos'; 
+
 
 const Stack = createNativeStackNavigator();
 
 export default function Menu({ navigation }) {
   const apertar = () => {
     navigation.navigate('eventos');
+    navigation.navigate('churrasqueria1');
+    navigation.navigate('churrasqueria2');
+    navigation.navigate('churrasqueira3');
   };
 
   return (
@@ -20,22 +23,25 @@ export default function Menu({ navigation }) {
       <Button
       title="Churrasqueira 1"
       color='#cd853f'
+      onPress={() => navigation.navigate('churrasqueria1')} 
       />
       <Text> </Text>
       <Button
       title="Churrasqueira 2"
       color='#cd853f'
+      onPress={() => navigation.navigate('churrasqueria2')} 
       />
       <Text> </Text>
       <Button
       title="Churrasqueira 3"
       color='#cd853f'
+      onPress={() => navigation.navigate('churrasqueira3')} 
       />
       <Text> </Text>
       <Button
       title="Eventos"
       color='#ffa07a'
-      onPress={apertar} 
+      onPress={() => navigation.navigate('eventos')} 
       />
 
     </SafeAreaView>
